209ae2a93ca6ba08f1112b182af6697e99789dce
Maxime Chagnon <luffy-star-93@hotmail.fr>
2021-01-15 14:23:29 +0100

    récupération du fichier avec les mots

--- pendu.py
+++ pendu.py
@@ -8,3 +8,19 @@
 #   /||
 #  //||
 # ============
+
+
+
+
+import random
+file = open("liste_francais.txt")
+allText = file.read()
+# print(allText)
+words = list(map(str, allText.split()))
+
+# print random string
+random_words = random.choice(words)
+print(random_words)
+
+compteur = 0 
+# while compteur <= 5:
